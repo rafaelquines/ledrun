cac827576d7349ee287097e3a7c26de7caf52844
Rafael Quines <rafael.quines@totvs.com.br>
2017-02-20 07:57:13 -0300

    Added example

---
+++ examples/usage.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Require package
+var LedRun = require('ledrun');
+
+// Define a pin that led is connected on Raspberry PI
+var ledRunPin = 26;
+
+// Initialize package with led pin
+var ledRun = new LedRun(26);
+
+// Start blink each 150ms
+ledRun.start(150);
